bc542af017cfcd5a7f18e79e1fb602b718f53d2a
Danny Chan <dchan6513@gmail.com>
2019-10-01 15:31:16 -0400

    Implement render of React app component

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -5,5 +5,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hello, this is a test page!</h1>, root)
+  ReactDOM.render(<h1>Hello, React is working!</h1>, root)
 })
