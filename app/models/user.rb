ff896c90d139f988f0a2599db1877a466fee46ae
Danny Chan <dchan6513@gmail.com>
2019-10-01 14:46:16 -0400

    Fill in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,40 @@
 class User < ApplicationRecord
+  validates :name, :password_digest, presence: true
+
+  # Validation of a unique email address with a valid format (built into Ruby)
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+  
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user && user.is_password(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
 end
