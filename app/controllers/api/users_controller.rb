8d1d2033c927e121db136c8427b067b3faf6dcce
Danny Chan <dchan6513@gmail.com>
2019-10-01 15:16:42 -0400

    Implement UsersController and add routes for User and Session

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+
+      # Once a user has successfully created their account and logged in,
+      # redirect to the Boards index page
+      # redirect to '/' 
+    else
+      render @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create] # Users can only create an account for now
+    resource :session, only: [:create, :destroy] # A user's session can be created or destroyed
+  end
 end
