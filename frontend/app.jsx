0593829b10485461cd8fe8478a7df22c44266075
Danny Chan <dchan6513@gmail.com>
2019-10-01 14:07:08 -0400

    Configure settings files

---
+++ frontend/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store'; // Doesn't exist yet
+// import Root from './components/root'; // Doesn't exist yet
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root />, root)
+})
