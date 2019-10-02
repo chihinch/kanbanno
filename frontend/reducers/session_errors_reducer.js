671759f7c64f40daa0e56b4fd288ca1d75f7c6e5
Danny Chan <dchan6513@gmail.com>
2019-10-02 00:16:01 -0400

    Fix typo in function definition of sessionErrorsReducer

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,6 +1,6 @@
 import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
-const sessionErrorsReducer = (state = [], action) {
+const sessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_SESSION_ERRORS:
