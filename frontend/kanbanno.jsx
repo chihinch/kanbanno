2a27a1d8b62551352f5b3ada265f2568d2b17d0b
Danny Chan <dchan6513@gmail.com>
2019-10-02 09:37:00 -0400

    Add App and Root components

--- frontend/kanbanno.jsx
+++ frontend/kanbanno.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store'; 
-// import Root from './components/root'; 
+import Root from './components/root'; 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
+
+  // TEST
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  ReactDOM.render(<h1>Hello, React is working!</h1>, root)
+  // TEST
+
+  ReactDOM.render(<Root store={store} />, root)
 })
