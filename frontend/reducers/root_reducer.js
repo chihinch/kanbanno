e475868ff2a2b4d2d6d73819ae564bf5d6313ff4
Danny Chan <dchan6513@gmail.com>
2019-10-02 00:22:47 -0400

    Add session key to rootReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 
+import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 
+// TODO: add usersReducer
+
 const rootReducer = combineReducers({
+  session: sessionReducer,
   errors: errorsReducer
 });
 
