c8f5b271c4362438127cb8d8d148be1f47155457
Danny Chan <dchan6513@gmail.com>
2019-10-02 00:16:18 -0400

    Create the store

--- frontend/kanbanno.jsx
+++ frontend/kanbanno.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './store/store'; // Doesn't exist yet
-// import Root from './components/root'; // Doesn't exist yet
+import configureStore from './store/store'; 
+// import Root from './components/root'; 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Hello, React is working!</h1>, root)
 })
