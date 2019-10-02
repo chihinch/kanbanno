7182377558578f7437eec4e4dafc5d3e6da82439
Danny Chan <dchan6513@gmail.com>
2019-10-02 00:13:50 -0400

    Create store with plugin to exclude logger from production

---
+++ frontend/store/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+// Exclude redux-logger from production
+if (process.env.NODE_ENV !== "production") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) => {
+  return createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
+};
+
+export default configureStore;
+
