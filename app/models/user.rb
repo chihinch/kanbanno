c7971194ec25a715e43dcb0caabec9f1729c5185
Danny Chan <dchan6513@gmail.com>
2019-10-03 10:18:51 -0400

    Fix bug in user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
     return nil unless user && user.is_password?(password)
+    user
   end
 
   def self.generate_session_token
