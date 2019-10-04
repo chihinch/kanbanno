503a60d05638b6c375f9745813e9447a637bf897
Danny Chan <dchan6513@gmail.com>
2019-10-04 14:01:28 -0400

    Tweak styling of first section of splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -25,23 +25,27 @@ export default class Splash extends React.Component {
 
       <section id="splash-1">
         <div id="splash-1-container">
-          <div className="splash-1-pitch">
-            <h1>
-              Kanbanno helps you streamline your workflow and get stuff done.
-            </h1>
-            <p>
-              Kanbanno's boards, lists, and cards provide an intuitive system to declutter your mind and organize your tasks.
-            </p>
+          <div>
+            <div className="splash-1-pitch">
+              <h1>
+                Kanbanno helps you streamline your workflow and get stuff done.
+              </h1>
+              <p>
+                Kanbanno's boards, lists, and cards provide an intuitive system to declutter your mind and organize your tasks.
+              </p>
+            </div>
+
+            <div className="splash-1-img">
+              <img src="https://cdn.pixabay.com/photo/2019/06/18/05/46/kanban-4281474_960_720.jpg" alt="kanban-example" />
+            </div>
           </div>
+
 
           <div className="splash-1-buttons">
             <Link to="/signup" className="splash-1-button">Sign Up - It's Free!</Link>
             <Link to="/" className="splash-1-button" onClick={this.props.demoLogin}>See How It Works!</Link>
           </div>
 
-          <div className="splash-1-img">
-            <img src="https://cdn.pixabay.com/photo/2019/06/18/05/46/kanban-4281474_960_720.jpg" alt="kanban-example"/>
-          </div>
         </div>
       </section>
 
