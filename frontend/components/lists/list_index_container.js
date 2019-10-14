0653565ebf81d811e1519cad977bc292f5685d95
Danny Chan <dchan6513@gmail.com>
2019-10-14 12:12:13 -0400

    Begin setup for ListIndex component

---
+++ frontend/components/lists/list_index_container.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+
+import { fetchLists } from '../../actions/list_actions';
+import ListIndex from './list_index';
+
+const mapStateToProps = (state, ownProps) => {
+  // const boardId = parseInt(ownProps.match.params.boardId);
+  return {
+    lists: Object.keys(state.entities.lists).map((id) => state.entities.lists[id])
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchLists: () => dispatch(fetchLists())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListIndex);
