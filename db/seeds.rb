997df7cda7ceb3169b6afa12b095e22a6c1a8708
Danny Chan <dchan6513@gmail.com>
2019-11-12 16:39:08 -0500

    Fill up seed boards with lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,18 +21,6 @@ b2 = {
 }
 
 b3 = {
-  title: "Creature of Fenkenstrain", 
-  description: "Dr Fenkenstrain, master and sole occupant of the castle to the north east of Canifis, needs a new servant to go on a dark errand for him. Do you have the stomach to help Fenkenstrain complete his twisted purpose?", 
-  admin_id: 1
-}
-
-b4 = {
-  title: "Tears of Guthix", 
-  description: "Deep in the caves in the Lumbridge Swamp is an enchanted place where the tears shed by Guthix when it saw the destruction Saradomin and Zamorak had caused with their wars flow from the very walls. These tears are said to have magical properties to help players gain deeper understanding of the world. However, the cave is guarded by a loyal serpent named Juna who blocks passage from everyone. However, she has grown bored from three thousand years of sitting in the dark cave and wishes to hear stories of life above. Maybe you could come to some sort of arrangement...?", 
-  admin_id: 1
-}
-
-b5 = {
   title: "One Small Favour", 
   description: "If you're new to Shilo Village after solving the mystery (or even if you did it some time ago), you may like to check out Yanni Salika's antiques shop.\n
   He's a busy chap, revamping antique items, checking out new stock from potential adventurers and maybe he'll have a small(1) favour to ask of you? Probably nothing particularly large, Yanni isn't a demanding(2) person, should hardly take you any time(3) at all really, something to do when you have a spare moment.\n
@@ -43,7 +31,7 @@ b5 = {
   admin_id: 1
 }
 
-seed_boards = Board.create([b1, b2, b3, b4, b5])
+seed_boards = Board.create([b1, b2, b3])
 
 b1_l1 = {
   title: "To Do",
@@ -66,22 +54,22 @@ b1_seed_lists[1].updateNeighbours(b1_seed_lists[0].id, b1_seed_lists[2].id)
 b1_seed_lists[2].updateNeighbours(b1_seed_lists[1].id, nil)
 
 b2_l1 = {
-  title: "Apple",
+  title: "To Do",
   board_id: seed_boards.second.id,
 }
 
 b2_l2 = {
-  title: "Tomato",
+  title: "Doing",
   board_id: seed_boards.second.id,
 }
 
 b2_l3 = {
-  title: "Corn",
+  title: "Done",
   board_id: seed_boards.second.id,
 }
 
 b2_l4 = {
-  title: "Milk",
+  title: "Quest Items",
   board_id: seed_boards.second.id,
 }
 
@@ -90,3 +78,23 @@ b2_seed_lists[0].updateNeighbours(nil, b2_seed_lists[1].id)
 b2_seed_lists[1].updateNeighbours(b2_seed_lists[0].id, b2_seed_lists[2].id)
 b2_seed_lists[2].updateNeighbours(b2_seed_lists[1].id, b2_seed_lists[3].id)
 b2_seed_lists[3].updateNeighbours(b2_seed_lists[2].id, nil)
+
+b3_l1 = {
+  title: "To Do",
+  board_id: seed_boards.third.id,
+}
+
+b3_l2 = {
+  title: "Doing",
+  board_id: seed_boards.third.id,
+}
+
+b3_l3 = {
+  title: "Done",
+  board_id: seed_boards.third.id,
+}
+
+b3_seed_lists = List.create([b3_l1, b3_l2, b3_l3])
+b3_seed_lists[0].updateNeighbours(nil, b3_seed_lists[1].id)
+b3_seed_lists[1].updateNeighbours(b3_seed_lists[0].id, b3_seed_lists[2].id)
+b3_seed_lists[2].updateNeighbours(b3_seed_lists[1].id, nil)
