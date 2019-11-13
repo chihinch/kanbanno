87d0974b2beddf80891d98055bb5463aa77d2c35
Danny Chan <dchan6513@gmail.com>
2019-11-12 20:13:29 -0500

    Fill in Card methods and create empty card components

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,13 +4,50 @@ class Card < ApplicationRecord
 
   belongs_to :list
 
-   def prev_card
-    return Card.new() if !self.prev_card_id
-    Card.find(self.prev_card_id)
+  def prev_card
+    Card.find_by(id: self.prev_card_id)
   end
 
-   def next_card
-    return Card.new() if !self.next_card_id
-    Card.find(self.next_card_id)
+  def next_card
+    Card.find_by(id: self.next_card_id)
+  end
+
+  def updateNeighbours(prev_card_id = nil, next_card_id = nil)
+
+    old_prev_card = self.prev_card
+    old_next_card = self.next_card
+
+    new_prev_card = Card.find_by(id: prev_card_id)
+    new_next_card = Card.find_by(id: next_card_id)
+    
+    # Introduce the new neighbours to this card
+    new_prev_card.next_card_id = self.id unless new_prev_card.nil?
+    new_next_card.prev_card_id = self.id unless new_next_card.nil?
+    
+    # Change the neighbours for this card
+    self.prev_card_id = prev_card_id
+    self.next_card_id = next_card_id
+
+    # And update the old neighbours
+    if !old_prev_card.nil? 
+      if !old_next_card.nil? # Left+ Right+
+        old_prev_card.next_card_id = old_next_card.id
+        old_next_card.prev_card_id = old_prev_card.id
+      else # Left+ Right-
+        old_prev_card.next_card_id = nil
+      end
+    else
+      if !old_next_card.nil? # Left- Right+
+        old_next_card.prev_card_id = nil
+      end
+    end
+
+    Card.transaction do
+      old_prev_card.save unless old_prev_card.nil?
+      old_next_card.save unless old_next_card.nil?
+      new_prev_card.save unless new_prev_card.nil?
+      new_next_card.save unless new_next_card.nil?
+      self.save
+    end
   end
 end
