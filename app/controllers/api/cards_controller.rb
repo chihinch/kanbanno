9b055f5de9cd3cc61b9cac4212a67c3c02ddca90
Danny Chan <dchan6513@gmail.com>
2019-11-18 15:39:28 -0500

    Fix CardsController#update to handle changes in title and desc

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -29,11 +29,16 @@ class Api::CardsController < ApplicationController
 
   def update
     card = Card.find(params[:id])
-    old_list_id = card.list_id
     if card
-      card.update_attribute(:list_id, card_params[:list_id])
-      card.updateNeighbours(card_params[:prev_card_id], card_params[:next_card_id])
-      @cards = Card.where(list_id: [card.list_id, old_list_id], archived: false)
+      if card_params[:list_id]
+        old_list_id = card.list_id
+        card.update_attribute(:list_id, card_params[:list_id])
+        card.updateNeighbours(card_params[:prev_card_id], card_params[:next_card_id])
+        @cards = Card.where(list_id: [card.list_id, old_list_id], archived: false)
+      else
+        card.update_attributes({title: card_params[:title], description: card_params[:description]})
+        @cards = Card.where(list_id: card.list_id, archived: false)
+      end
       render :index
     else
       render json: card.errors.full_messages, status: 404
