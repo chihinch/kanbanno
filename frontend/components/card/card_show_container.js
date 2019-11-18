e7573d540e77d2da2b29336613a3d0baf9b169ab
Danny Chan <dchan6513@gmail.com>
2019-11-18 14:29:20 -0500

    Fill in the rest of msp and mdp in cardShow

--- frontend/components/card/card_show_container.js
+++ frontend/components/card/card_show_container.js
@@ -1,13 +1,21 @@
 import { connect } from 'react-redux';
 
 import CardShow from './card_show';
+import { updateCard } from '../../actions/card_actions'
 
 const mapStateToProps = (state, ownProps) => {
   debugger
+  const cardId = ownProps.cardId;
+  const card = state.entities.cards[cardId];
+  return {
+    card
+  }
 };
 
 const mapDispatchToProps = (dispatch) => {
-
+  return {
+    updateCard: (card) => dispatch(updateCard(card))
+  }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CardShow);
