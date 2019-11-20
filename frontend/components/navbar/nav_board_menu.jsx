36d1c9ebe2d3408e7b58f67351119b4892e0bd83
Danny Chan <dchan6513@gmail.com>
2019-11-20 15:25:53 -0500

    Update navbar board menu to show personal and shared boards

--- frontend/components/navbar/nav_board_menu.jsx
+++ frontend/components/navbar/nav_board_menu.jsx
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom';
 import onClickOutside from 'react-onclickoutside';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 import { fetchBoards } from '../../actions/board_actions';
 import { openModal } from '../../actions/modal_actions';
 import { closeMenu } from '../../actions/menu_actions';
 
-const mapStateToProps = ({ entities }) => {
+const mapStateToProps = (state) => {
   return {
-    boards: entities.boards
+    boards: Object.keys(state.entities.boards).map((id) => state.entities.boards[id])
   };
 };
 
@@ -26,6 +26,11 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 class NavBoardMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.constructBoards = this.constructBoards.bind(this);
+  }
+
   handleClickOutside(e) {
     this.props.closeMenu();
   }
@@ -33,9 +38,17 @@ class NavBoardMenu extends React.Component {
   componentDidMount() {
     this.props.fetchBoards();
   }
-  
-  render() {
-    const boardItems = Object.values(this.props.boards).map((board) => {
+
+  constructBoards(type) {
+    let selectedBoards;
+    if (type === 'personal') {
+      selectedBoards = this.props.boards.filter((board) => board.members.length === 1);
+    }
+    else if (type === 'shared') {
+      selectedBoards = this.props.boards.filter((board) => board.members.length > 1)
+    }
+
+    const boardItems = selectedBoards.map((board) => {
       return (
         <div className="board-li" key={`personal-${board.id}`}>
           <Link to={`/boards/${board.id}`} onClick={this.props.closeMenu} className="board-li-link">
@@ -45,9 +58,17 @@ class NavBoardMenu extends React.Component {
             </div>
           </Link>
         </div>
-      )
+      );
     });
 
+    return (
+      <div className="board-menu-section-list">
+        {boardItems}
+      </div>
+    );
+  }
+  
+  render() {
     return (
       <div className="board-menu-container">
         <div className="board-menu-lists">
@@ -58,10 +79,17 @@ class NavBoardMenu extends React.Component {
                 <span className="board-menu-section-title">Personal Boards</span>
               </span>
             </div>
+            {this.constructBoards('personal')}
+          </div>
 
-            <div className="board-menu-section-list">
-              {boardItems}
+          <div className="board-menu-section" id="shared-boards">
+            <div className="board-menu-section-header">
+              <span>
+                <span className="board-menu-section-icon"><FontAwesomeIcon icon={faUsers} /></span>
+                <span className="board-menu-section-title">Shared Boards</span>
+              </span>
             </div>
+            {this.constructBoards('shared')}
           </div>
         </div>
         <div className="board-menu-options">
