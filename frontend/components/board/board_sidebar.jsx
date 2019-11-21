b15415f7059a354314742fd1a072fb20c2ba6c8b
Danny Chan <dchan6513@gmail.com>
2019-11-21 17:34:58 -0500

    Add option to edit board to sidebar

--- frontend/components/board/board_sidebar.jsx
+++ frontend/components/board/board_sidebar.jsx
@@ -1,18 +1,22 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import React from 'react';
 
+import { openModal } from '../../actions/modal_actions';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+  const boardId = parseInt(ownProps.match.params.boardId);
   return {
-
+    boardId,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    openUpdateBoardModal: (id) => dispatch(openModal('updateBoard', id)),
   };
 };
 
@@ -30,10 +34,21 @@ class BoardSidebar extends React.Component {
   render() {
     return (
       <div className="board-sidebar" id="board-sidebar">
-        <span onClick={this.closeBoardSidebar}><FontAwesomeIcon icon={faTimes} /></span>
+        <div className="board-sidebar-header">
+          <span className="board-sidebar-title">Menu</span>
+          <span className="menu-close" onClick={this.closeBoardSidebar}><FontAwesomeIcon icon={faTimes} /></span>
+        </div>
+        <div className="board-sidebar-content">
+          <div className="board-sidebar-options">
+            <a to="#" className="board-sidebar-option" id="sidebar-edit" onClick={() => this.props.openUpdateBoardModal(this.props.boardId)}>
+              <span><FontAwesomeIcon icon={faBars} /></span>
+              Edit Board
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(BoardSidebar);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(BoardSidebar));
