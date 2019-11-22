b4c49700f15aa5ab9f10377c239542b3567108d1
Danny Chan <dchan6513@gmail.com>
2019-11-21 23:44:50 -0500

    Update footer links to my online presence

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIdBadge, faDoorOpen } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faAngellist, faGithub, faLinkedin, faTrello } from '@fortawesome/free-brands-svg-icons';
+
 export default class Footer extends React.Component {
 
   render () {
     return (
     <footer>
       <ul>
-        <li key="footer-li-1"><Link to="#" onClick={this.props.demoLogin}>Tour</Link></li>
-        <li key="footer-li-2"><Link to="#">Pricing</Link></li>
-        <li key="footer-li-3"><Link to="#">Apps</Link></li>
-        <li key="footer-li-4"><Link to="#">Jobs</Link></li>
-        <li key="footer-li-5"><Link to="#">Blog</Link></li>
-        <li key="footer-li-6"><Link to="#">Developers</Link></li>
-        <li key="footer-li-7"><Link to="#">About</Link></li>
-        <li key="footer-li-8"><Link to="#">Help</Link></li>
-        <li key="footer-li-9"><Link to="#">Legal</Link></li>
-        <li key="footer-li-10"><Link to="#">Cookie Settings</Link></li>
-        <li key="footer-li-11"><Link to="#">Privacy</Link></li>
+        <li key="footer-li-1"><a href="#" onClick={this.props.demoLogin}><FontAwesomeIcon icon={faDoorOpen} />Tour</a></li>
+        <li key="footer-li-2"><a href="https://dannychan.dev"><FontAwesomeIcon icon={faIdBadge} />Portfolio</a></li>
+        <li key="footer-li-3"><a href="https://github.com/chihinch/kanbanno"><FontAwesomeIcon icon={faGithub} />GitHub</a></li>
+        <li key="footer-li-4"><a href="https://linkedin.com/in/chihinchan"><FontAwesomeIcon icon={faLinkedin} />LinkedIn</a></li>
+        <li key="footer-li-5"><a href="https://angel.co/chihinch"><FontAwesomeIcon icon={faAngellist} />AngelList</a></li>
+        <li key="footer-li-6"><a href="mailto:chihinch@outlook.com"><FontAwesomeIcon icon={faEnvelope} />Email Me</a></li>
       </ul>
 
       <div>
-        <h1>Kanbanno</h1>
-        <p>© Copyright 2019. All rights reserved.</p>
+        <h1>Kanbanno by Danny Chan</h1>
+        <a href="https://trello.com">Inspired by <FontAwesomeIcon icon={faTrello} />Trello</a>
       </div>
     </footer>
     );
