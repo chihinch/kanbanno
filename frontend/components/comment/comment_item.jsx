48b0d7a792bbbc6c2f9f051e0602603ca0d88c26
Danny Chan <dchan6513@gmail.com>
2019-11-22 23:58:39 -0500

    Style appearance of comment author, date, body

--- frontend/components/comment/comment_item.jsx
+++ frontend/components/comment/comment_item.jsx
@@ -25,15 +25,20 @@ class CommentItem extends React.Component {
   }
 
   render() {
+    const created = new Date(this.props.comment.created_at);
     const updated = new Date(this.props.comment.updated_at);
+    const elapsed = created - updated;
+
     const readableUpdated = updated.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' });
+    const edited = elapsed > 1000 ? "(edited)" : ""
+    const dateShown = readableUpdated + edited;
 
     return (
       <li className="comment-list-item">
         <div className="comment-item-container">
           <div className="comment-item-header">
             <span id="comment-item-author">{this.props.author}</span>
-            <span id="comment-item-updatedat">{readableUpdated}</span>
+            <span id="comment-item-updatedat">{dateShown}</span>
           </div>
           <div className="comment-item-body">
             <p>{this.state.body}</p>
